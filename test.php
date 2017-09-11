d924b9d31a3ca5dd99ecfb5c2c3fc42df1b30784
J. Hölzle <j.hoelzle@exinit.de>
2017-09-11 19:03:05 +0200

    [TASK] added pull test to project

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Jan-Vincent Hölzle (j.hoelzle@exinit.de), exinit GmbH & Co. KG
+ *
+ *  All rights reserved
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Project:    deploybot-test
+ * Description:
+ *
+ * @author:    Jan-Vincent Hölzle <j.hoelzle@exinit.de>
+ * Date:        11.09.2017 19:02
+ */
